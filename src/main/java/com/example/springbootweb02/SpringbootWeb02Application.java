246a92ddc7b7819eb3f5e1874f7910ca123999ee
vanbou <resumebb@163.com>
2022-04-06 15:45:23 +0800

    前端界面提交

---
+++ src/main/java/com/example/springbootweb02/SpringbootWeb02Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootweb02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWeb02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWeb02Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springbootweb02/test.java
@@ -0,0 +1,12 @@
+package com.example.springbootweb02;
+
+/**
+ * Description :
+ * Created by Resumebb
+ * Date :2022/3/30
+ */
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
